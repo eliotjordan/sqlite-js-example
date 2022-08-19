54758a3b25ea99e71d20b9556f23b55eb0635b61
Eliot Jordan <eliotj@princeton.edu>
2022-08-19 11:24:09 -0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { createDbWorker } from "sql.js-httpvfs";
+
+const workerUrl = new URL(
+  "sql.js-httpvfs/dist/sqlite.worker.js",
+  import.meta.url
+);
+const wasmUrl = new URL("sql.js-httpvfs/dist/sql-wasm.wasm", import.meta.url);
+
+async function load() {
+  const worker = await createDbWorker(
+    [
+      {
+        from: "inline",
+        config: {
+          serverMode: "full",
+          url: "/Chinook.db",
+          requestChunkSize: 4096,
+        },
+      },
+    ],
+    workerUrl.toString(),
+    wasmUrl.toString()
+  );
+
+  const result = await worker.db.query(`select Composer, count(*) as counter from Track where Composer is not null group by Composer order by counter`);
+
+  document.body.textContent = JSON.stringify(result);
+}
+
+load();
